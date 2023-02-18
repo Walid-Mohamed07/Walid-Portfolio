53047393d3e37d3bfba43cfb13e2de8ff03baacb
Walid Mohamed <walidmohamed234@gmail.com>
2023-02-19 00:38:55 +0200

    first commit 1.0

---
+++ src/components/About/index.js
@@ -0,0 +1,66 @@
+import './index.scss';
+import AnimatedLetters from '../AnimatedLetters';
+import { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngular, faCss3, faGitAlt, faHtml5, faJsSquare, faReact } from '@fortawesome/free-brands-svg-icons';
+import Loader from 'react-loaders';
+
+const About = () => {
+    const [letterClass, setLetterClass] = useState('text-animate');
+
+    useEffect(() => {
+        setTimeout(() => {
+           setLetterClass('text-animate-hover');
+        }, 3000);
+    }, []);
+
+    return (
+        <>
+            <div className='container about-page'>
+                <div className='text-zone'>
+                    <h1>
+                        <AnimatedLetters
+                            letterClass={letterClass}
+                            strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+                            idx={15}
+                        />
+                    </h1>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi quisquam est minima quaerat, neque saepe iure corporis praesentium eligendi, impedit similique veniam cum illum eum ducimus libero voluptas amet facere?
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum beatae perferendis, eos quas aspernatur, accusamus voluptates nisi impedit, earum porro corporis eius soluta reprehenderit facere eum velit officiis? Nostrum, illum.
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, magnam eos qui rem incidunt aspernatur a ab quo nobis esse voluptatibus omnis consequuntur laudantium corrupti quaerat enim. Nostrum, eius placeat.
+                    </p>
+                </div>
+
+                <div className='stage-cube-count'>
+                    <div className='cubespinner'>
+                        <div className='face1'>
+                            <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+                        </div>
+                        <div className='face2'>
+                            <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+                        </div>
+                        <div className='face3'>
+                            <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+                        </div>
+                        <div className='face4'>
+                            <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+                        </div>
+                        <div className='face5'>
+                            <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+                        </div>
+                        <div className='face6'>
+                            <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Loader type='pacman' />
+        </>
+    )
+}
+export default About;
---
+++ src/firebase.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAoVgT0f_8ONNVx_CkCRY6ao4g0YHhbAKs",
+  authDomain: "walid-portfolio-23e48.firebaseapp.com",
+  projectId: "walid-portfolio-23e48",
+  storageBucket: "walid-portfolio-23e48.appspot.com",
+  messagingSenderId: "966187473157",
+  appId: "1:966187473157:web:8506528cd4c9c93e0e7f6c",
+  measurementId: "G-6ST5ZYQ9EM"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const auth = getAuth();
+const provider = new GoogleAuthProvider();
+
+export const signInWithGoogle = () => signInWithPopup(auth, provider);
